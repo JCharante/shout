de994b065442d0e9caa863d8ef137663e88d1150
JCharante <git@jcharante.com>
2018-12-01 17:59:17 -0500

    test

--- index.py
+++ index.py
@@ -228,7 +228,9 @@ def incoming_sms():
             ]))
             return str(response)
     elif textBody[0:22] == "https://www.google.com":
+        print(textBody)
         expandedUrl = unshorten_url(textBody.split(' ')[0])
+        print(expandedUrl)
         temp = re.search('@([0-9]?[0-9]\.[0-9]*),([0-9]?[0-9]\.[0-9]*)', expandedUrl, re.DOTALL)
         latitude = temp.groups()[0]
         longitude = temp.groups()[1]
