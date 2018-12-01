24f3a9500545efde1707d2eadeff1af9d8b973c7
JCharante <git@jcharante.com>
2018-12-01 12:50:23 -0500

    add user logic

--- index.py
+++ index.py
@@ -1,4 +1,22 @@
 import os
+from sqlalchemy import Column, Integer, String, Boolean, create_engine, Text, DATETIME, JSON, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+Base = declarative_base()
+
+class UserV1(Base):
+    __tablename__ = 'UserV1'
+    phoneNumber = Column(Text)
+    shoutRange = Column(Integer)
+    haveSignedUp = Column(Boolean)
+
+
+engine = create_engine(os.environ['DBA'])
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+
 from flask import Flask, request, redirect
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
@@ -12,6 +30,25 @@ app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
+    session = DBSession()
+    phoneNumberFromTwilio = request.values.get('From', None)
+    if phoneNumberFromTwilio is None:
+        return "Stop forging requests"
+
+    userInDB = session.query(UserV1).filter_by(phoneNumber=phoneNumberFromTwilio).first()
+    if userInDB is None:
+        session.add(UserV1(
+            phoneNumber=phoneNumberFromTwilio,
+            shoutRange=-1,
+            haveSignedUp=False
+        ))
+        session.commit()
+        session.close()
+        response = MessagingResponse()
+        response.message("Hey this is Shout! Do you want to sign up to receive shouts? If so, reply w/ SIGNUP")
+        return str(response)
+
+    """
     body = request.values.get('Body', None)
     message = client.messages \
         .create(
@@ -19,6 +56,7 @@ def incoming_sms():
         from_=os.environ['SHOUT_NUM'],
         to=os.environ['TRANG_NUM']
     )
+    """
     resp = MessagingResponse()
     resp.message("I don't know how to reply to your text message")
 
